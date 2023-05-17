28212e23528fac83ff544c12893a886060397ed3
Pavel Kornienkov <101476878+CapriKorP@users.noreply.github.com>
2023-05-17 13:21:27 +0300

    Add tests Wrong name and phone

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
@@ -38,7 +40,7 @@ class cardOrderTest {
 
     @Test
     void happyPathTest() {
-        driver.get("http://localhost:9999/");
+        driver.get("http://localhost:7777/"); //на локальном хосте запускать 7777
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79999999999");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
@@ -47,4 +49,40 @@ class cardOrderTest {
         String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
         assertEquals(expected, actual);
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Ivan Ivanov",
+            "Иван 1 Иванов",
+            "*:(?:*?(*%"
+    })
+    void wrongNameTest(String name) {
+        driver.get("http://localhost:7777/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys(name);
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource ({
+        "+7999999999",
+        "79999999999",
+        "-79999991234",
+        "++9998881234"
+    })
+    void wrongPhoneTest(String number) {
+        driver.get("http://localhost:7777/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванов");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys(number);
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
 }
